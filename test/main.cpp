b7bd6d01f57e59e4e0391b64f0cffba5da9a27cf
proxict <bobogu@protonmail.com>
2021-05-18 02:23:09 +0200

    Added unit tests

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+#include "cpplibsocket/Socket.h"
+
+#include <gmock/gmock.h>
+
+using namespace cpplibsocket;
+
+TEST(SocketTest, basic) {
+    Socket<IPProto::TCP>(IPVer::IPV4);
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleMock(&argc, argv);
+    ::testing::FLAGS_gtest_death_test_style = "threadsafe";
+    return RUN_ALL_TESTS();
+}
